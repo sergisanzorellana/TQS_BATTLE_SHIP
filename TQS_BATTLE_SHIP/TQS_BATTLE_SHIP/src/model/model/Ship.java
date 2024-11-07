e80d21f8dfdd438464eff635e6b448e78752f0a3
sergisanzorellana <sergisanzorellana@gmail.com>
2024-11-07 11:20:48 +0100

    Ship Test and fuction done

---
+++ TQS_BATTLE_SHIP/TQS_BATTLE_SHIP/src/model/model/Ship.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Ship {
+    private int size;
+    private String name;
+
+    public Ship(int size, String name) {
+        this.size = size;
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- TQS_BATTLE_SHIP/TQS_BATTLE_SHIP/src/test/test/ShipTest.java
+++ TQS_BATTLE_SHIP/TQS_BATTLE_SHIP/src/test/test/ShipTest.java
@@ -1,5 +1,14 @@
 package test;
 
-public class ShipTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import model.Ship;
 
+public class ShipTest {
+    @Test
+    public void testShipAttributes() {
+        Ship ship = new Ship(3, "Destroyer");
+        assertEquals(3, ship.getSize());
+        assertEquals("Destroyer", ship.getName());
+    }
 }
