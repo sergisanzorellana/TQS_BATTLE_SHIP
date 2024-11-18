d7ea96fd6b5a8d942a5ea5f77e1c0ab755e79e03
heinz <heinz@10.109.40.100>
2024-11-18 12:11:06 +0100

    Ship class totally implemented

--- TQS_BATTLE_SHIP/TQS_BATTLE_SHIP/src/model/model/Ship.java
+++ TQS_BATTLE_SHIP/TQS_BATTLE_SHIP/src/model/model/Ship.java
@@ -3,10 +3,13 @@ package model;
 public class Ship {
     private int size;
     private String name;
+    private boolean sunk;
+    private int[][] occupiedSquares;
 
     public Ship(int size, String name) {
         this.size = size;
         this.name = name;
+        this.sunk = false;
     }
 
     public int getSize() {
@@ -15,6 +18,35 @@ public class Ship {
 
     public String getName() {
         return name;
+    }
+
+    public boolean isSunk() {
+        return sunk;
+    }
+
+    public void setOccupiedSquares(int[][] occupiedSquares) {
+        this.occupiedSquares = occupiedSquares;
+    }
+
+    public boolean hit(int x, int y) {
+        for (int i = 0; i < size; i++) {
+            if (occupiedSquares[i][0] == x && occupiedSquares[i][1] == y) {
+                occupiedSquares[i][0] = -1; // Mark as hit
+                occupiedSquares[i][1] = -1;
+                checkSunk();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkSunk() {
+        for (int i = 0; i < size; i++) {
+            if (occupiedSquares[i][0] != -1) {
+                return;
+            }
+        }
+        sunk = true;
     }
 }
 
